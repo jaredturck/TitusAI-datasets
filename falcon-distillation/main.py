1000be892df764e20cbf171d538e14e77cc7ed96
Jared Turck <jaredturck8@gmail.com>
2025-11-18 01:24:15 +0000

    Added prompt generation

--- falcon-distillation/main.py
+++ falcon-distillation/main.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer
 import transformers
-import torch
+import torch, random
 
 class FalconDistillation:
     ''' Create training samples using Falcon '''
@@ -15,16 +15,35 @@ class FalconDistillation:
             torch_dtype=torch.bfloat16,
             device_map="cuda",
         )
+        self.no_examples = 2
+        self.prompt = open('prompt_template.txt','r').read()
+        self.starters = open('prompt_conversation_starts.txt').read().split('\n')
+        self.topics = open('prompt_conversation_topics.txt').read().split('\n')
+    
+    def generate_prompt(self):
+
+        starter_text = ', '.join(self.starters[i] for i in random.sample(range(0, len(self.starters)), self.no_examples))
+        topics_text = ', '.join(self.topics[i] for i in random.sample(range(0, len(self.topics)), self.no_examples))
+
+        return self.prompt.format(
+            conv_topics = topics_text,
+            conv_starters = starter_text
+        )
     
     def generate(self):
 
         sequences = self.pipeline(
-        "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
-            max_length=200,
+            self.generate_prompt(),
+            max_new_tokens=1024,
             do_sample=True,
-            top_k=10,
+            top_k=50,
+            top_p=0.9,
+            temperature=0.8,
             num_return_sequences=1,
             eos_token_id=self.tokenizer.eos_token_id,
         )
-        for seq in sequences:
-            print(f"Result: {seq['generated_text']}")
+
+        print(sequences[0]['generated_text'])
+
+dt = FalconDistillation()
+dt.generate()
