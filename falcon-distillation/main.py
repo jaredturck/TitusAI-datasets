0e861f97007b5830a29fa69d77b1826313b568fc
Jared Turck <jaredturck8@gmail.com>
2025-11-18 00:31:25 +0000

    Added prompt seeds

--- falcon-distillation/main.py
+++ falcon-distillation/main.py
@@ -2,23 +2,29 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 
-model = "tiiuae/falcon-7b"
+class FalconDistillation:
+    ''' Create training samples using Falcon '''
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    device_map="cuda",
-)
-sequences = pipeline(
-   "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
-    max_length=200,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-)
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+    def __init__(self):
+        self.model = "tiiuae/falcon-7b"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model)
+        self.pipeline = transformers.pipeline(
+            "text-generation",
+            model=self.model,
+            tokenizer=self.tokenizer,
+            torch_dtype=torch.bfloat16,
+            device_map="cuda",
+        )
+    
+    def generate(self):
+
+        sequences = self.pipeline(
+        "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+            max_length=200,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=self.tokenizer.eos_token_id,
+        )
+        for seq in sequences:
+            print(f"Result: {seq['generated_text']}")
