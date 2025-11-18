d99c8acd78cba395c7dccbb8d6b377284f97fc8d
Jared Turck <jaredturck8@gmail.com>
2025-11-18 02:47:37 +0000

    Changed model hyper-parameters

--- falcon-distillation/main.py
+++ falcon-distillation/main.py
@@ -6,7 +6,7 @@ class FalconDistillation:
     ''' Create training samples using Falcon '''
 
     def __init__(self):
-        self.model = "tiiuae/falcon-7b"
+        self.model = "tiiuae/falcon-7b-instruct"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model)
         self.pipeline = transformers.pipeline(
             "text-generation",
@@ -15,33 +15,36 @@ class FalconDistillation:
             torch_dtype=torch.bfloat16,
             device_map="cuda",
         )
-        self.no_examples = 2
         self.prompt = open('prompt_template.txt','r').read()
         self.starters = open('prompt_conversation_starts.txt').read().split('\n')
         self.topics = open('prompt_conversation_topics.txt').read().split('\n')
     
     def generate_prompt(self):
 
-        starter_text = ', '.join(self.starters[i] for i in random.sample(range(0, len(self.starters)), self.no_examples))
-        topics_text = ', '.join(self.topics[i] for i in random.sample(range(0, len(self.topics)), self.no_examples))
+        starter_text = random.choice(self.starters)
+        topics_text = random.choice(self.topics)
 
-        return self.prompt.format(
-            conv_topics = topics_text,
-            conv_starters = starter_text
-        )
+        return f'{self.prompt} for example {starter_text} {topics_text}'
     
     def generate(self):
 
         sequences = self.pipeline(
             self.generate_prompt(),
-            max_new_tokens=1024,
             do_sample=True,
-            top_k=50,
+            top_k=10,
             top_p=0.9,
-            temperature=0.8,
-            num_return_sequences=1,
+            temperature=1.2,
+            num_return_sequences=50,
+            repetition_penalty=1.1,
+            no_repeat_ngram_size=4,
+            max_length=2048,
+            return_full_text=False,
             eos_token_id=self.tokenizer.eos_token_id,
         )
+
+        with open('output_chat.txt', 'w') as file:
+            for seq in sequences:
+                file.write('\n[BOS]\n' + seq['generated_text'] + '\n[EOS]\n')
 
         print(sequences[0]['generated_text'])
 
