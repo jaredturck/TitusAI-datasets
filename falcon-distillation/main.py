043b67dffc5e5222c5bee517d16c0d9d3a39d9b7
Jared Turck <jaredturck8@gmail.com>
2025-11-20 02:21:27 +0000

    Added code for generate 3

--- falcon-distillation/main.py
+++ falcon-distillation/main.py
@@ -42,9 +42,9 @@ class FalconDistillation:
         txt = re.sub(r'^(person|person a|person b|person one|person two|friend|user|you|bot)[\s]*[\d+:\s]*', '', txt, flags=re.IGNORECASE|re.MULTILINE)
         txt = re.sub(r'^\d+[\.\):]*', '', txt, flags=re.IGNORECASE|re.MULTILINE)
         return txt
-    
+
     def generate(self):
-        
+        ''' Dataset 1 and 2 '''
         seed = random.randrange(0, 2**32-1)
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
@@ -72,8 +72,14 @@ class FalconDistillation:
         send_status(f'[+] Wrote sequences to {fname}')
     
     def generate_2(self):
+        ''' Dataset 3 '''
+        self.output_path = 'outputs_dataset_3/'
         try:
-            for prompt in self.topics + self.starters:
+            for n,prompt in enumerate(self.topics + self.starters):
+                if n < 580:
+                    print(f'[+] Skipped prompt {prompt}')
+                    continue
+
                 seed = random.randrange(0, 2**32-1)
                 torch.manual_seed(seed)
                 torch.cuda.manual_seed_all(seed)
@@ -99,7 +105,49 @@ class FalconDistillation:
                 send_status(f'[+] Wrote sequences to {fname}')
         except Exception as e:
             send_status(f'[error] Exception during generation: {e}')
+    
+    def process_pipeline(self, prompt, num_return_sequences):
+        seed = random.randrange(0, 2**32-1)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        print(f'[+] prompt: {prompt}')
+        sequences = self.pipeline(
+            prompt,
+            do_sample=True,
+            top_k=50,
+            top_p=0.9,
+            temperature=0.8,
+            num_return_sequences=num_return_sequences,
+            repetition_penalty=1.1,
+            no_repeat_ngram_size=4,
+            return_full_text=True,
+            eos_token_id=self.tokenizer.eos_token_id,
+        )
+        
+        fname = f'output_chat_{datetime.datetime.now().strftime("%d_%b_%Y_%H_%M")}_{seed}.txt'
+        with open(os.path.join(self.output_path, fname), 'a', encoding='utf-8') as file:
+            for seq in sequences:
+                file.write('\n' + self.clean_up_text(seq['generated_text']) + '\n')
+
+        send_status(f'[+] Wrote sequences to {fname}')
+    
+    def generate_3(self):
+        ''' Dataset 4 '''
+        self.output_path = 'outputs_dataset_4/'
+        questions = open('prompt_questions.txt').read().split('\n')
+        open_ended_questions = open('prompt_open_ended_questions.txt').read().split('\n')
+        try:
+            for prompt in questions:
+                self.process_pipeline(prompt, num_return_sequences=50) # 50 samples lower quality questions
+            
+            for prompt in open_ended_questions:
+                self.process_pipeline(prompt, num_return_sequences=200) # 200 samples higher quality open ended questions
+                
+        except Exception as e:
+            send_status(f'[error] Exception during generation: {e}')
 
 if __name__ == '__main__':
     dt = FalconDistillation()
-    dt.generate_2()
+    # dt.generate() - dataset 1 and 2
+    dt.generate_2() # dataset 3
+    dt.generate_3() # dataset 4
